a1f0e6817ec8581a69134e10d04f17af784395bb
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-12-07 23:09:54 +0800

    Add remove-duplicates-from-sorted-array
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/26-remove-duplicates-from-sorted-array.c
+++ src/26-remove-duplicates-from-sorted-array.c
@@ -13,14 +13,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <assert.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
 
 int removeDuplicates(int* nums, int numsSize) {
+        if (0 == numsSize) return 0;
 
+        int ptr = 0;
+        for (int i = 0; i < numsSize; i++) {
+                if (nums[ptr] != nums[i])
+                        nums[++ptr] = nums[i];
+        }
+        return (ptr + 1);
+}
+
+// unittest with cmocka
+static void verify(int* nums, int numsSize, int* expect, int expectSize)
+{
+        assert_int_equal(removeDuplicates(nums, numsSize), expectSize);
+        for (int i = 0; i < expectSize; i++) {
+                assert_int_equal(nums[i], expect[i]);
+        }
+}
+
+#define ASIZE(x)  (sizeof(x) / sizeof((x)[0]))
+static void Test0(void **state)
+{
+        int t1[] = {1, 1, 2, 3}, t1e[] = {1, 2, 3};
+        verify(t1,  ASIZE(t1),    // nums, numsSize
+               t1e, ASIZE(t1e));  // expect, expectSize
+
+        int t2[] = {1, 2, 2, 2, 3, 5}, t2e[] = {1, 2, 3, 5};
+        verify(t2,  ASIZE(t2),    // nums, numsSize
+               t2e, ASIZE(t2e));  // expect, expectSize
 }
 
 int main(int argc, char *argv[])
 {
+        const struct CMUnitTest tests[] = {
+                cmocka_unit_test(Test0)
+        };
 
-        return 0;
+        return cmocka_run_group_tests(tests, NULL, NULL);
 }
