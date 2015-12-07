d86327c580f8a07b08c4d03855af7a8cf7e731e7
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-12-07 21:14:06 +0800

    update
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ algorithms/83-remove-duplicates-from-sorted-list.c
@@ -0,0 +1,37 @@
+/* https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ *
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ *
+ * For example,
+ *   Given 1->1->2, return 1->2.
+ *   Given 1->1->2->3->3, return 1->2->3.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct ListNode {
+        int val;
+        struct ListNode *next;
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+        return 0;
+}
